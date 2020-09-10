555c7cc2a2a5af5bfb2c5f3088bdbfb5adbbde8c
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-09-11 00:23:50 +0700

    How to render elements

--- src/App.js
+++ src/App.js
@@ -3,15 +3,63 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Hau';
+  const age = 18;
+  const isMale = true;
+  const student = {
+    name: 'Easy Frontend'
+  };
+  const colorList = ['red', 'green', 'blue'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hau Nguyen
+          Hau Nguyen 123
         </p>
+
+        <p>Xin chao {name} - {age} - {isMale ? 'Male' : 'Female'}</p>
+
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && <p>Male</p>}
+        {!isMale && <p> Female</p>}
+
+
+        {isMale && (
+          <div>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </div>
+        )}
+
+        {isMale && (
+          <React.Fragment>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </React.Fragment>
+        )}
+
+        {isMale && (
+          <>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </>
+        )}
+
+        <p>{student.name}</p>
+
+        <ul>
+          {colorList.map(color => (
+            <li key={color} style={{ color }}>{color}</li>
+          ))}
+        </ul>
       </header>
-    </div>
+    </div >
   );
 }
 
