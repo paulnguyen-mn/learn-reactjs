6da5411ecb2b646f6bd36d1d1cacfd2cc0beb2d0
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-10-17 23:55:22 +0700

    Add react-router-dom lib

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
+import AlbumFeature from "./features/Album";
 import TodoFeature from "./features/Todo";
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      Header
+      <p><NavLink to="/todos" activeClassName="active-menu">Todos</NavLink></p>
+      <p><NavLink to="/albums" activeClassName="active">Albums</NavLink></p>
+
+      <Switch>
+        <Route path="/" component={TodoFeature} exact />
+        <Route path="/todos" component={TodoFeature} />
+        <Route path="/albums" component={AlbumFeature} />
+      </Switch>
+
+      Footer
     </div>
   );
 }
