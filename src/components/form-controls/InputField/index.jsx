29922dfe2079dde54d643655f3351cb34b8da6a8
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-10-19 09:41:47 +0700

    Add yup validation

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -13,6 +13,8 @@ InputField.propTypes = {
 
 function InputField(props) {
   const { form, name, label, disabled } = props;
+  const { errors, formState } = form;
+  const hasError = formState.touched[name] && errors[name];
 
   return (
     <Controller
@@ -23,6 +25,9 @@ function InputField(props) {
       fullWidth
       label={label}
       disabled={disabled}
+
+      error={!!hasError}
+      helperText={errors[name]?.message}
     />
   );
 }
--- src/features/Todo/components/TodoForm/index.jsx
+++ src/features/Todo/components/TodoForm/index.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import PropTypes from 'prop-types';
-import InputField from '../../../../components/form-controls/InputField';
+import React from 'react';
 import { useForm } from 'react-hook-form';
+import * as yup from "yup";
+import InputField from '../../../../components/form-controls/InputField';
 
 TodoForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
 function TodoForm(props) {
+  const schema = yup.object().shape({
+    title: yup.string()
+      .required('Please enter title')
+      .min(5, 'Title is too short'),
+  });
   const form = useForm({
     defaultValues: {
       title: '',
     },
+    resolver: yupResolver(schema),
   });
 
   const handleSubmit = (values) => {
