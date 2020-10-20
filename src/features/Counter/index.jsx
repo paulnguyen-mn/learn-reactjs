0340b1904c2fe4517d296b89afc5cad76ad73903
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-10-20 14:58:08 +0700

    Using css module

--- src/features/Counter/index.jsx
+++ src/features/Counter/index.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { decrease, increase } from './counterSlice';
+import styles from './styles.module.css';
 
-CounterFeature.propTypes = {
-
-};
+CounterFeature.propTypes = {};
 
 function CounterFeature(props) {
   const dispatch = useDispatch();
-  const count = useSelector(state => state.count);
+  const count = useSelector((state) => state.count);
 
   const handleIncreaseClick = () => {
     const action = increase(); // action creator
     dispatch(action);
-  }
+  };
 
   const handleDecreaseClick = () => {
     // const action = decrease(); // action creator
     dispatch(decrease());
-  }
+  };
 
   return (
-    <div>
+    <div className={styles.count}>
       Counter: {count}
-
       <div>
         <button onClick={handleIncreaseClick}>Increase</button>
         <button onClick={handleDecreaseClick}>Decrease</button>
