f04c0cd92a0cf03746dd4c4fa93b0726da8be5cd
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-10-20 14:07:27 +0700

    Add jsconfig

--- src/features/Todo/components/TodoForm/index.jsx
+++ src/features/Todo/components/TodoForm/index.jsx
@@ -1,9 +1,9 @@
 import { yupResolver } from '@hookform/resolvers/yup';
+import InputField from 'components/form-controls/InputField';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
-import InputField from '../../../../components/form-controls/InputField';
 
 TodoForm.propTypes = {
   onSubmit: PropTypes.func,
