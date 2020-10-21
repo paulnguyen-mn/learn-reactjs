23df23924a1c3d6e536ba59604fce6dfb8878b71
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-10-21 16:51:43 +0700

    Use async action: register

--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -1,11 +1,28 @@
+import { unwrapResult } from '@reduxjs/toolkit';
+import { register } from 'features/Auth/userSlice';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import RegisterForm from '../RegisterForm';
 
 Register.propTypes = {};
 
 function Register(props) {
-  const handleSubmit = (values) => {
-    console.log('Form Submit:', values);
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (values) => {
+    try {
+      // auto set username = email
+      values.username = values.email;
+
+      const action = register(values);
+      const resultAction = await dispatch(action);
+      const user = unwrapResult(resultAction);
+
+      // do something here on register successfully
+      console.log('New user', user);
+    } catch (error) {
+      console.log('Failed to register:', error);
+    }
   };
 
   return (
