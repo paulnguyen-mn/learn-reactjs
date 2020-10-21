64532fb402898ea0e460ce4c62215ea9b4bde2de
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-10-21 10:20:47 +0700

    Add PasswordField control

---
+++ src/components/form-controls/PasswordField/index.jsx
@@ -0,0 +1,56 @@
+import FormControl from '@material-ui/core/FormControl';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { Controller } from 'react-hook-form';
+
+PasswordField.propTypes = {
+  form: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+function PasswordField(props) {
+  const { form, name, label, disabled } = props;
+  const { errors, formState } = form;
+  const hasError = formState.touched[name] && errors[name];
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword((x) => !x);
+  };
+
+  return (
+    <FormControl fullWidth margin="normal" variant="outlined">
+      <InputLabel htmlFor={name}>{label}</InputLabel>
+      <Controller
+        name={name}
+        control={form.control}
+        as={OutlinedInput}
+        id={name}
+        type={showPassword ? 'text' : 'password'}
+        label={label}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton aria-label="toggle password visibility" onClick={toggleShowPassword} edge="end">
+              {showPassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        }
+        disabled={disabled}
+        error={!!hasError}
+        helperText={errors[name]?.message}
+      />
+    </FormControl>
+  );
+}
+
+export default PasswordField;
--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -2,6 +2,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { Avatar, Button, makeStyles, Typography } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
 import InputField from 'components/form-controls/InputField';
+import PasswordField from 'components/form-controls/PasswordField';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -34,9 +35,7 @@ RegisterForm.propTypes = {
 function RegisterForm(props) {
   const classes = useStyles();
 
-  const schema = yup.object().shape({
-    title: yup.string().required('Please enter title').min(5, 'Title is too short'),
-  });
+  const schema = yup.object().shape({});
   const form = useForm({
     defaultValues: {
       fullName: '',
@@ -69,10 +68,10 @@ function RegisterForm(props) {
       <form onSubmit={form.handleSubmit(handleSubmit)}>
         <InputField name="fullName" label="Full Name" form={form} />
         <InputField name="email" label="Email" form={form} />
-        <InputField name="password" label="Password" form={form} />
-        <InputField name="retypePassword" label="Retype Password" form={form} />
+        <PasswordField name="password" label="Password" form={form} />
+        <PasswordField name="retypePassword" label="Retype Password" form={form} />
 
-        <Button className={classes.submit} variant="contained" color="primary" fullWidth>
+        <Button type="submit" className={classes.submit} variant="contained" color="primary" fullWidth>
           Create an account
         </Button>
       </form>
