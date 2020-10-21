5e2d155b1f716c9e762f5cfad0bb7f1867717ffc
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-10-21 13:56:30 +0700

    Create async action: register
    - Update jsconfig to use es6 module

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,4 +1,15 @@
-const { createSlice } = require('@reduxjs/toolkit');
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import userApi from 'api/userApi';
+
+const register = createAsyncThunk('user/register', async (payload) => {
+  const data = await userApi.register(payload);
+
+  // save data to local storage
+  localStorage.setItem('access_token', data.jwt);
+  localStorage.setItem('user', JSON.stringify(data.user));
+
+  return data.user;
+});
 
 const userSlice = createSlice({
   name: 'user',
@@ -7,6 +18,11 @@ const userSlice = createSlice({
     settings: {},
   },
   reducers: {},
+  extraReducers: {
+    [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    },
+  },
 });
 
 const { reducer } = userSlice;
