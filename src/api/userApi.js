110639552302165143a2dc326c0aec6fad0bd3ee
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-10-22 18:09:54 +0700

    Add login api and action

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -5,6 +5,11 @@ const userApi = {
     const url = '/auth/local/register';
     return axiosClient.post(url, data);
   },
+
+  login(data) {
+    const url = '/auth/local';
+    return axiosClient.post(url, data);
+  },
 };
 
 export default userApi;
--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -11,6 +11,16 @@ export const register = createAsyncThunk('user/register', async (payload) => {
   return data.user;
 });
 
+export const login = createAsyncThunk('user/login', async (payload) => {
+  const data = await userApi.login(payload);
+
+  // save data to local storage
+  localStorage.setItem('access_token', data.jwt);
+  localStorage.setItem('user', JSON.stringify(data.user));
+
+  return data.user;
+});
+
 const userSlice = createSlice({
   name: 'user',
   initialState: {
@@ -20,6 +30,10 @@ const userSlice = createSlice({
   reducers: {},
   extraReducers: {
     [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    },
+
+    [login.fulfilled]: (state, action) => {
       state.current = action.payload;
     },
   },
