42a2918e536436ead9a8b493048fae577ef40a43
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-12-12 11:48:39 +0700

    112 - Setup listing layout

--- src/features/Product/index.jsx
+++ src/features/Product/index.jsx
@@ -1,3 +1,4 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import ListPage from './pages/ListPage';
@@ -6,12 +7,11 @@ function ProductFeature(props) {
   const match = useRouteMatch();
 
   return (
-    <div>
-      Product Feature
+    <Box pt={4}>
       <Switch>
         <Route path={match.url} exact component={ListPage} />
       </Switch>
-    </div>
+    </Box>
   );
 }
 
--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,7 +1,36 @@
+import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles((theme) => ({
+  root: {},
+
+  left: {
+    width: '250px',
+  },
+
+  right: {
+    flex: '1 1 auto',
+  },
+}));
+
 function ListPage(props) {
-  return <div>Product Listing Page</div>;
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <Container>
+        <Grid container spacing={1}>
+          <Grid item className={classes.left}>
+            <Paper elevation={0}>Left column</Paper>
+          </Grid>
+
+          <Grid item className={classes.right}>
+            <Paper elevation={0}>Right column</Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
 
 export default ListPage;
