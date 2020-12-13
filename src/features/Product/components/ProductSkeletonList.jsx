00acabfce21dc6c81d806a3b82832203495e606d
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-12-13 11:57:18 +0700

    114 - Product skeletons

---
+++ src/features/Product/components/ProductSkeletonList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Grid } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+
+ProductSkeletonList.propTypes = {
+  length: PropTypes.number,
+};
+
+ProductSkeletonList.defaultProps = {
+  length: 6,
+};
+
+function ProductSkeletonList({ length }) {
+  return (
+    <Box>
+      <Grid container>
+        {Array.from(new Array(length)).map((x, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+            <Box padding={1}>
+              <Skeleton variant="rect" width="100%" height={118} />
+              <Skeleton />
+              <Skeleton width="60%" />
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+}
+
+export default ProductSkeletonList;
--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,6 +1,7 @@
-import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
+import { Box, Container, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
 import productApi from 'api/productApi';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductSkeletonList from '../components/ProductSkeletonList';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -16,11 +17,19 @@ const useStyles = makeStyles((theme) => ({
 
 function ListPage(props) {
   const classes = useStyles();
+  const [productList, setProductList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
-      const response = await productApi.getAll({ _page: 1, _limit: 10 });
-      console.log({ response });
+      try {
+        const { data } = await productApi.getAll({ _page: 1, _limit: 10 });
+        setProductList(data);
+      } catch (error) {
+        console.log('Failed to fetch product list: ', error);
+      }
+
+      // setLoading(false);
     })();
   }, []);
 
@@ -33,7 +42,7 @@ function ListPage(props) {
           </Grid>
 
           <Grid item className={classes.right}>
-            <Paper elevation={0}>Right column</Paper>
+            <Paper elevation={0}>{loading ? <ProductSkeletonList /> : <Typography>Product List</Typography>}</Paper>
           </Grid>
         </Grid>
       </Container>
