5f7e3f3e5b2050b46a8238cf47e826b64f935d58
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-03-14 13:38:06 +0700

    125 - Filter by Price UI

--- src/features/Product/components/Filters/FilterByPrice.jsx
+++ src/features/Product/components/Filters/FilterByPrice.jsx
@@ -1,12 +1,34 @@
-import { Box, Button, TextField, Typography } from '@material-ui/core';
+import { Box, Button, makeStyles, TextField, Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    borderTop: `1px solid ${theme.palette.grey[300]}`,
+  },
+
+  range: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    alignItems: 'center',
+
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+
+    '& > span': {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+  },
+}));
 
 FilterByPrice.propTypes = {
   onChange: PropTypes.func,
 };
 
 function FilterByPrice({ onChange }) {
+  const classes = useStyles();
   const [values, setValues] = useState({
     salePrice_gte: 0,
     salePrice_lte: 0,
@@ -30,16 +52,16 @@ function FilterByPrice({ onChange }) {
   };
 
   return (
-    <Box>
-      <Typography variant="subtitle2">GIÁ</Typography>
+    <Box className={classes.root}>
+      <Typography variant="subtitle2">CHỌN KHOẢNG GIÁ</Typography>
 
-      <Box>
+      <Box className={classes.range}>
         <TextField name="salePrice_gte" value={values.salePrice_gte} onChange={handleChange} />
         <span>-</span>
         <TextField name="salePrice_lte" value={values.salePrice_lte} onChange={handleChange} />
       </Box>
 
-      <Button variant="outlined" color="primary" onClick={handleSubmit}>
+      <Button variant="outlined" color="primary" size="small" onClick={handleSubmit}>
         Áp dụng
       </Button>
     </Box>
