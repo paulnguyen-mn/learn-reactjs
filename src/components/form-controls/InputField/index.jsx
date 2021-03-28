3ee3c8d952cf24babc090d8ed4749346e79fed36
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-03-28 12:01:04 +0700

    137 - Update RHF packages
    - Refactor InputField + PasswordField

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { Controller } from 'react-hook-form';
 
 InputField.propTypes = {
@@ -20,14 +20,21 @@ function InputField(props) {
     <Controller
       name={name}
       control={form.control}
-      as={TextField}
-      margin="normal"
-      variant="outlined"
-      fullWidth
-      label={label}
-      disabled={disabled}
-      error={!!hasError}
-      helperText={errors[name]?.message}
+      render={({ onChange, onBlur, value, name }) => (
+        <TextField
+          margin="normal"
+          variant="outlined"
+          fullWidth
+          label={label}
+          disabled={disabled}
+          error={!!hasError}
+          helperText={errors[name]?.message}
+          name={name}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      )}
     />
   );
 }
--- src/components/form-controls/PasswordField/index.jsx
+++ src/components/form-controls/PasswordField/index.jsx
@@ -32,21 +32,28 @@ function PasswordField(props) {
   return (
     <FormControl error={hasError} fullWidth margin="normal" variant="outlined">
       <InputLabel htmlFor={name}>{label}</InputLabel>
+
       <Controller
         name={name}
         control={form.control}
-        as={OutlinedInput}
-        id={name}
-        type={showPassword ? 'text' : 'password'}
-        label={label}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton aria-label="toggle password visibility" onClick={toggleShowPassword} edge="end">
-              {showPassword ? <Visibility /> : <VisibilityOff />}
-            </IconButton>
-          </InputAdornment>
-        }
-        disabled={disabled}
+        render={({ onChange, onBlur, value, name }) => (
+          <OutlinedInput
+            id={name}
+            type={showPassword ? 'text' : 'password'}
+            label={label}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton aria-label="toggle password visibility" onClick={toggleShowPassword} edge="end">
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            disabled={disabled}
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
+        )}
       />
 
       <FormHelperText>{errors[name]?.message}</FormHelperText>
