12addabbc007f17cea78d14892bd8229ef94e15c
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-04-02 23:19:01 +0700

    146 - Cart items actions

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import counterReducer from '../features/Counter/counterSlice';
 import userReducer from '../features/Auth/userSlice';
+import cartReducer from '../features/Cart/cartSlice';
 const { configureStore } = require('@reduxjs/toolkit');
 
 const rootReducer = {
   count: counterReducer,
   user: userReducer,
+  cart: cartReducer,
 };
 
 const store = configureStore({
--- src/features/Cart/cartSlice.js
+++ src/features/Cart/cartSlice.js
@@ -14,6 +14,34 @@ const cartSlice = createSlice({
     hideMiniCart(state) {
       state.showMiniCart = false;
     },
+
+    addToCart(state, action) {
+      // newItem = { id, product, quantity }
+      const newItem = action.payload;
+      const index = state.cartItems.findIndex((x) => x.id === newItem.id);
+
+      if (index >= 0) {
+        // increase quantity
+        state.cartItems[index].quantity += newItem.quantity;
+      } else {
+        // add to cart
+        state.cartItems.push(newItem);
+      }
+    },
+
+    setQuantity(state, action) {
+      const { id, quantity } = action.payload;
+      // check if product is available in cart
+      const index = state.cartItems.findIndex((x) => x.id === id);
+      if (index >= 0) {
+        state.cartItems[index].quantity = quantity;
+      }
+    },
+
+    removeFromCart(state, action) {
+      const idNeedToRemove = action.payload;
+      state.cartItems = state.cartItems.filter((x) => x.id !== idNeedToRemove);
+    },
   },
 });
 
