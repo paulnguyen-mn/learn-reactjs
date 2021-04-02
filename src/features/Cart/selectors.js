603fb32e3807da22c45c72d11de15df1bac92048
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-04-02 23:33:41 +0700

    147 - createSelector

---
+++ src/features/Cart/selectors.js
@@ -0,0 +1,13 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+const cartItemsSelector = (state) => state.cart.cartItems;
+
+// Count number of products in cart
+export const cartItemsCountSelector = createSelector(cartItemsSelector, (cartItems) =>
+  cartItems.reduce((count, item) => count + item.quantity, 0)
+);
+
+// Calculate total of cart
+export const cartTotalSelector = createSelector(cartItemsSelector, (cartItems) =>
+  cartItems.reduce((total, item) => total + item.salePrice * item.quantity, 0)
+);
